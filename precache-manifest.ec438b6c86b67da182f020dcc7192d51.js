7a86ce1123c85e01de8980ed5dcc13732cdffab9
dingq84 <dingq84@gmail.com>
2019-08-11 21:20:54 +0800

    Updates

---
+++ precache-manifest.ec438b6c86b67da182f020dcc7192d51.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9abc630507ac9b0f8a97134e4b575d3f",
+    "url": "/online-payment/index.html"
+  },
+  {
+    "revision": "2e3dc89bf3624cb9fd24",
+    "url": "/online-payment/static/css/main.93214c39.chunk.css"
+  },
+  {
+    "revision": "9b7beb394a30df86be73",
+    "url": "/online-payment/static/js/2.f076b6c0.chunk.js"
+  },
+  {
+    "revision": "2e3dc89bf3624cb9fd24",
+    "url": "/online-payment/static/js/main.0e05c27d.chunk.js"
+  },
+  {
+    "revision": "f2af4cb5fab43c61bcf7",
+    "url": "/online-payment/static/js/runtime~main.61fc5e8b.js"
+  },
+  {
+    "revision": "69c7ef2ffde5a54b02d83c8581f41280",
+    "url": "/online-payment/static/media/headphoneIMGL4494_TP_V4.69c7ef2f.png"
+  },
+  {
+    "revision": "d328eefa43aa3883a903540bcb50ce73",
+    "url": "/online-payment/static/media/icon_JCB.d328eefa.svg"
+  },
+  {
+    "revision": "ccee46c76ec4a7f065642b81211edc0a",
+    "url": "/online-payment/static/media/icon_Master.ccee46c7.svg"
+  },
+  {
+    "revision": "dbdb4bbc9ea7419c10e59e8b8c146096",
+    "url": "/online-payment/static/media/icon_PayByATM.dbdb4bbc.svg"
+  },
+  {
+    "revision": "3205d3a4974743914a7f1d38a037866b",
+    "url": "/online-payment/static/media/icon_PayByCard.3205d3a4.svg"
+  },
+  {
+    "revision": "e80fbf555092851761c261327fc786b6",
+    "url": "/online-payment/static/media/icon_PayByShop.e80fbf55.svg"
+  },
+  {
+    "revision": "af25752d6c128610fc1f633aa1c6857f",
+    "url": "/online-payment/static/media/icon_VISA.af25752d.svg"
+  },
+  {
+    "revision": "90237a7bf17f896ab8f43b927b7e7d41",
+    "url": "/online-payment/static/media/icon_notice.90237a7b.svg"
+  },
+  {
+    "revision": "0b175cbbe15d6b78ca88abb317cd7981",
+    "url": "/online-payment/static/media/icon_question.0b175cbb.svg"
+  },
+  {
+    "revision": "399839f1ab426d86682af1886d46ae46",
+    "url": "/online-payment/static/media/ipadpro-9_TP_V4.399839f1.png"
+  },
+  {
+    "revision": "f1ffbcbc0d1a242c65344bc62b4258b9",
+    "url": "/online-payment/static/media/order_btn_delete_normal.f1ffbcbc.svg"
+  }
+]);
